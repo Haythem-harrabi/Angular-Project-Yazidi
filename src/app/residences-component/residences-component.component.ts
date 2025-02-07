f07fd43e1f61b314e84e84f7fafea2ef760b68f9
haythem harrabi <harrabihaithem21@gmail.com>
2025-02-07 21:00:57 +0100

    first commit

---
+++ src/app/residences-component/residences-component.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { Residence } from '../core/models/residence';
+
+@Component({
+  selector: 'app-residences-component',
+  templateUrl: './residences-component.component.html',
+  styleUrls: ['./residences-component.component.css']
+})
+export class ResidencesComponentComponent {
+
+  listResidences:Residence[]=[
+    {id:1,"name": "El fel","address":"Borj Cedria", "image":"../../assets/images/R1.jpeg", status: "Disponible"},
+     {id:2,"name": "El yasmine", "address":"Ezzahra","image":"../../assets/images/R2.jpg", status: "Disponible" },
+     {id:3,"name": "El Arij", "address":"Rades","image":"../../assets/images/R3.jpg", status: "Vendu"},
+     {id:4,"name": "El Anber","address":"inconnu", "image":"../../assets/images/R4.jpg", status: "En Construction"}
+   ];
+ 
+   Favorites:Residence[]=[];
+   Filtered:Residence[]=[{id:1,"name": "El fel","address":"Borj Cedria", "image":"../../assets/images/R1.jpeg", status: "Disponible"},
+    {id:2,"name": "El yasmine", "address":"Ezzahra","image":"../../assets/images/R2.jpg", status: "Disponible" },
+    {id:3,"name": "El Arij", "address":"Rades","image":"../../assets/images/R3.jpg", status: "Vendu"},
+    {id:4,"name": "El Anber","address":"inconnu", "image":"../../assets/images/R4.jpg", status: "En Construction"}];
+   searchInput : string | undefined ;
+  
+
+   ShowAddress( p : any){
+    if (p.address=="inconnu"){
+      alert("Adresse inconnue !!")
+    }
+    else{
+      alert(p.address)
+    }
+   }
+
+   AddFavorite(i : any){
+    this.Favorites.push(this.listResidences[i])
+    document.getElementById("Likebtn")?.setAttribute("disabled", "true");
+    
+   }
+   search(){
+      this.Filtered = this.listResidences.filter(residence => 
+        residence.address.toLowerCase().includes(this.searchInput?.toLowerCase() || '')
+      );
+      
+    }
+}
